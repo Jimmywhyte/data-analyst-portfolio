a850df67e5cecbcc9a26a631e889c91bda82dd12
Jimmywhyte <james4cricket@gmail.com>
2026-07-14 23:38:45 +0100

    Added back what mistakely deleted

--- sql-practice/q01-matching-skills.sql
+++ sql-practice/q01-matching-skills.sql
@@ -5,5 +5,7 @@ LinkedIn SQL Interview Question*/
 SELECT candidate_Id
 FROM candidates
 WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+    GROUP BY candidate_id
     HAVING count(skill) = 3
-    ORDER BY candidate_id ASC
+    ORDER BY candidate_id
+    
